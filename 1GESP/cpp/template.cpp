5cbb7e72f5e3c54b735eaca84c83780efc53823e
Lakkie <lakkie@qq.com>
2026-08-03 19:58:24 +0800

    Add cpp

--- 1GESP/cpp/template.cpp
+++ 1GESP/cpp/template.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    # 输入
-	int n;
+    // 输入
+	int n,s=0;
 	cin>>n;
 
-    # 计算
+    // 计算
     for(int i=0;i<n;i++){
-        
+        s+=i;
     }
-    
-    # 输出
-	cout<<n<<endl;
+
+    // 输出
+	cout<<s<<endl;
+
     return 0;
 }
